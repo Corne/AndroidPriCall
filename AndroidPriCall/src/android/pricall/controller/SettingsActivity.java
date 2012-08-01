837758277a8439a9608824a2968572fe5ec6e51c
Corne <cornevbeek123@live.nl>
2012-08-01 14:28:28 +0200

    app no longer closes on back button pressed

--- AndroidPriCall/src/android/pricall/controller/SettingsActivity.java
+++ AndroidPriCall/src/android/pricall/controller/SettingsActivity.java
@@ -5,6 +5,7 @@ package android.pricall.controller;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.pricall.R;
 import android.util.Log;
@@ -18,6 +19,8 @@ import android.widget.ToggleButton;
  */
 public class SettingsActivity extends Activity implements OnClickListener {
 
+	private static final String TOGGLEBUTTONPREFERENCE = "ToggleButtonPreference";
+	
 	private ToggleButton activeStatusToggleButton;
 	
 	@Override
@@ -25,19 +28,50 @@ public class SettingsActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settingsview);
 		
+		//get the objects from the view
 		activeStatusToggleButton = (ToggleButton) findViewById(R.id.app_status_togglebutton);
+		//set OnClickListener
 		activeStatusToggleButton.setOnClickListener(this);
+		//set 'saved' toggleButton value
+		SharedPreferences settings = getPreferences(0);
+		boolean checkedToggleButton = settings.getBoolean(TOGGLEBUTTONPREFERENCE, false);
+		activeStatusToggleButton.setChecked(checkedToggleButton);
+		//make sure he activates again when button is checked on startup
+		this.startPriCallService();
 	}
 
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		//Save ToggleButton value
+		SharedPreferences settings = getPreferences(0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putBoolean(TOGGLEBUTTONPREFERENCE, activeStatusToggleButton.isChecked());
+		editor.commit();
+	}
+	
+	@Override
+	public void onBackPressed() {
+		//make sure app doesn't close on back button pressed
+		Intent setIntent = new Intent(Intent.ACTION_MAIN);
+		setIntent.addCategory(Intent.CATEGORY_HOME);
+		setIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(setIntent);
+	}
+	
 	public void onClick(View v) {
 		if(v.getId() == activeStatusToggleButton.getId()){
-			if(activeStatusToggleButton.isChecked()){
-				Log.i("SERVICE", "onClick: starting service");
-				startService(new Intent(this, PriCallService.class));
-			}else{
-				Log.i("SERVICE", "onClick: stopping service");
-				stopService(new Intent(this, PriCallService.class));
-			}
+			this.startPriCallService();
+		}
+	}
+	
+	private void startPriCallService(){
+		if(activeStatusToggleButton.isChecked()){
+			Log.i("SERVICE", "onClick: starting service");
+			startService(new Intent(this, PriCallService.class));
+		}else{
+			Log.i("SERVICE", "onClick: stopping service");
+			stopService(new Intent(this, PriCallService.class));
 		}
 	}
 }
